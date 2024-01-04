0a7ca4f8317bf786a5b9071c328de5b45b48d8df
Ivan Barros <ivansilvabarros@outlook.com>
2024-01-03 22:11:06 -0300

    identação

--- Robo.Domain/Constants/QueriesTables/BracoQueryConstant.cs
+++ Robo.Domain/Constants/QueriesTables/BracoQueryConstant.cs
@@ -8,13 +8,11 @@ INNER JOIN cotovelo c ON c.Id = b.IdCotovelo
 INNER JOIN pulso p ON p.Id = b.IdPulso
 WHERE c.lado = @Lado AND p.lado = @Lado";
 
-
         public const string InsertStatusCotovelo = $@"INSERT INTO cotovelo (status, lado) values(@Status, @Lado)";
 
         public const string UpdateStatusCotovelo = $@"UPDATE cotovelo SET status = @Status Where lado = @Lado";
 
         public const string insertPulso = $@"INSERT into pulso (rotacao, lado) values (@Rotacao, @Lado)";
-
 
         public const string insereBracoDireito = $@"INSERT INTO braco (IdCotovelo,IdPulso)
 Values(@IdCotovelo,@IdPulso)";
